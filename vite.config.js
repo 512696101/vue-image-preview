ff2931902ccf99a90d0325e472a2c63ba234e84d
wangyuqing <512696101@qq.com>
2026-06-01 09:42:38 +0800

    feat:11

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true, // 在 package.json 自动插入 types 字段
+      include: ['src/lib/imagePreview'], // 指定源码目录
+    }),
+  ],
   build: {
+    target: 'es2015',
     lib: {
       entry: resolve(__dirname, 'src/lib/imagePreview/index.ts'),
       name: 'ImagePreview',
       formats: ['es', 'umd'],
-      fileName: (format) => `image-preview.${format === 'es' ? 'js' : 'umd.js'}`
-    }
-  }
+      fileName: (format) =>
+        `image-preview.${format === 'es' ? 'js' : 'umd.js'}`,
+    },
+    minify: 'terser',
+  },
 })
